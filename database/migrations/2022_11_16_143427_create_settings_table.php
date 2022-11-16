d62a424cf511bba4cb4c819110036551f9ba89eb
Ucheizzzy <uchechukwuobiakor@gmail.com>
2022-11-16 16:15:41 +0100

    Settings model and table created and migrated

---
+++ database/migrations/2022_11_16_143427_create_settings_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('settings', function (Blueprint $table) {
+            $table->id()->from(time());
+            $table->boolean('email_notification');
+            $table->boolean('mobile_push_notification');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('settings');
+    }
+};
