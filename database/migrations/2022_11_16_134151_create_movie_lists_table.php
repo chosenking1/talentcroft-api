9bde788fdf9077568d7597c8e73e5c704d0d18ca
chosenking1 <82238206+chosenking1@users.noreply.github.com>
2022-11-17 10:29:13 +0100

    model

---
+++ database/migrations/2022_11_16_134151_create_movie_lists_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('movie_lists', function (Blueprint $table) {
+            $table->id()->from(time());
+            $table->string('title');
+            $table->longtext('description')->nullable();
+            $table->string('type')->nullable();
+            $table->string('genre');
+            $table->json('content')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('movie_lists');
+    }
+};
