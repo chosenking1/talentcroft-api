3c7ce4b4c8aa1fe1626e763fdd1c3adb62b23e34
chosenking1 <82238206+chosenking1@users.noreply.github.com>
2022-11-23 16:37:07 +0100

    working Apis

--- routes/api.php
+++ routes/api.php
@@ -26,26 +26,23 @@ use Illuminate\Support\Facades\Route;
 */
 
 //public routes
-    /** AUTHENTICATION AND USER DETAILS */
-    Route::group(['prefix' => 'user'], function () {
-        // login
-        Route::post('/login', [UserController::class, 'login']);
-        // register
-        Route::post('/register', [UserController::class, 'register']);
-        Route::group(['middleware' => 'auth:api'], function () {
-            //get authenticated user
-            Route::get('/profile/{id}', [UserController::class, 'getAuthenticatedUser']);
-           
-        });
-         // delete user
-         Route::delete('/{id}', [UserController::class,'deleteUser']);
-        // update user credentials
-        Route::put('/update/{id}', [UserController::class, 'updateUser']);
-        //get all users
-        Route::get('/allusers', [UserController::class,'getAllUsers']);
-        //get a user
-        Route::get('/{id}', [UserController::class,'getUser']);
+/** AUTHENTICATION AND USER DETAILS */
+Route::group(['prefix' => 'user'], function () {
+    // login
+    Route::post('/login', [UserController::class, 'login']);
+    // register
+    Route::post('/register', [UserController::class, 'register']);
+    Route::group(['middleware' => 'auth:api'], function () {
+        //get authenticated user
+        Route::get('/profile/{id}', [UserController::class, 'getAuthenticatedUser']);
+
     });
-
-
-
+    // delete user
+    Route::delete('/{id}', [UserController::class,'deleteUser']);
+    // update user credentials
+    Route::put('/update/{id}', [UserController::class, 'updateUser']);
+    //get all users
+    Route::get('/allusers', [UserController::class,'getAllUsers']);
+    //get a user
+    Route::get('/{id}', [UserController::class,'getUser']);
+});
