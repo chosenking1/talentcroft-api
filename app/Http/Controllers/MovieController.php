06737822f7e2b2065935bfc59d2930d92a381c37
Ucheizzzy <uchechukwuobiakor@gmail.com>
2022-12-14 16:53:47 +0100

    upload file to amazon with store function and delete file from amazone with delete function tested and working

--- app/Http/Controllers/MovieController.php
+++ app/Http/Controllers/MovieController.php
@@ -54,5 +54,45 @@ class MovieController extends Controller
         $movie = Movie::destroy($id);
         return $this->respondWithSuccess('Deleted Successfully', 201);
     }
+
+    final public function store(Request $request){
+        $this->validate($request, ['url' => 'required']);
+        if($request->hasfile('url')){
+            $file = $request->file('url');
+            $name = time().$file->getClientOriginalName();
+            $filePath = 'movies/'.$name;
+            Storage::disk('s3')->put($filePath, file_get_contents($file));
+         }
+         //insert into database
+        $movie_files = new MovieFile();
+        $movie_files->movie_id = $request->movie_id;
+        $movie_files->url = $filePath;
+        // $movie_file->thumbnail = $filePath;
+        $movie_files->size = $request->size;
+        $movie_files->duration = $request->duration;
+        $movie_files->meta = $request->meta;
+        $movie_files->processed_at = Carbon::now();
+        $movie_files->save();
+
+        return $this->respondWithSuccess(['data' => ['message' => 'Movie inserted successfully', 'movie_file' =>   $movie_files]], 201);
+    }
+
+    final public function delete($id){
+        $movie_files = MovieFile::findorfail($id);
+
+        if(empty($movie_files)){
+            return response()->json(['success' => false, 'message' => 'Movie file not found'], 404);
+        }
+
+        //We remove existing movie files
+        if(!empty($movie_files))
+        {
+            Storage::disk('s3')->delete($movie_files->url);
+            $movie_files->delete();
+            return response()->json(['success' => true, 'message' => 'Movie file deleted Successfully'], 200);
+        }
+
+        return response()->json(['success' => false, 'message' => 'Unable to delete the Movie file. Please try again later.'], 400);
+    }
     
 }
